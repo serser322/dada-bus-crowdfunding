4c102255f8fee88e6f6d88a4f6ca0169f4957661
Wei Chiao Chen <www322.joe@gmail.com>
2023-12-15 00:26:26 +0800

    chore: git init

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./ui/Header";
+import Content from "./ui/Content";
+import Footer from "./ui/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/ui/Content.jsx
@@ -0,0 +1,9 @@
+function Content() {
+  return (
+    <div>
+      Content
+    </div>
+  )
+}
+
+export default Content
