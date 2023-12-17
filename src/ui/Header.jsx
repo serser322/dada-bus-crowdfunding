4191364b9f1b19972f532755e8019b7fba497e37
Wei Chiao Chen <www322.joe@gmail.com>
2023-12-17 15:51:25 +0800

    feat:Header切版

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,15 +4,23 @@ import { CloseRound, SendFilled } from "@ricons/material";
 const Container = styled.div``;
 // const LineGroup = styled.div``;
 const Line = styled.div`
-  height: 6px;
   width: 100vw;
+  height: ${(props) => (props.color === "red" ? "6px" : "3px")};
   background-color: ${(props) => props.color};
 
   @media (min-width: 640px) {
-    height: 10px;
-    width: ${(props) => (props.color === "red" ? "20vw" : "18vw")};
+    height: ${(props) => (props.color === "red" ? "10px" : "6px")};
+    width: ${(props) => (props.color === "red" ? "100%" : "90%")};
   }
 `;
+
+const TitleGroup = styled.div`
+  width: auto;
+  @media (min-width: 640px) {
+    min-width: 20rem;
+  }
+`;
+
 const TitleText = styled.p`
   font-size: 20px;
 
@@ -21,19 +29,14 @@ const TitleText = styled.p`
   }
 `;
 
-// const TitleIcon = styled.div`
-//   width: 40px;
-// `;
-// const TitleContainer = styled.div``;
-
 function Header() {
   return (
     <Container className="flex flex-col justify-between sm:flex-row sm:mt-6">
-      <div>
-        <Line color="red" className="my-1" />
+      <div className="sm:w-full">
+        <Line color="red" className="mb-1.5 mt-1" />
         <Line color="orange" />
       </div>
-      <div className="flex flex-col items-center font-bold mt-6 sm:mx-8 sm:mt-0">
+      <TitleGroup className="flex flex-col items-center font-bold mt-6 sm:items-start sm:mx-10 sm:mt-0">
         <div className="flex items-center space-x-4">
           <TitleText>
             <span className="font-extrabold text-4xl sm:text-5xl">灰妲</span>
@@ -43,17 +46,15 @@ function Header() {
           </div>
           <TitleText>三周年</TitleText>
         </div>
-        {/* <div className="sm:ml-24 mt-4"> */}
         <div className="flex items-center space-x-4 sm:ml-24 mt-4">
           <div className="w-5 sm:w-7">
             <SendFilled />
           </div>
           <TitleText>公車募資計畫</TitleText>
         </div>
-        {/* </div> */}
-      </div>
-      <div className="hidden sm:flex flex-col justify-end items-end pb-2">
-        <Line color="orange" className="my-1" />
+      </TitleGroup>
+      <div className="hidden sm:flex flex-col justify-end items-end pb-2 sm:w-full">
+        <Line color="orange" className="mb-1.5" />
         <Line color="red" />
       </div>
     </Container>
